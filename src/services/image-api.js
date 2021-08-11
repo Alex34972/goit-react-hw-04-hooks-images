f1105701e35e2fed2cf68179554b7ac43e1f0f3c
Alex34972 <alex_hifi@ukr.net>
2021-08-11 11:43:06 +0300

    Create image-api.js

---
+++ src/services/image-api.js
@@ -0,0 +1,20 @@
+const API_KEY = `21948852-6fcb2a22057a93c1128c532ff`;
+const BASE_URL = `https://pixabay.com/api`;
+
+function fetchImages(nextName, page) {
+  console.log(page);
+  const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${nextName}&page=${page}&per_page=12&key=${API_KEY}`;
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    return Promise.reject(new Error('No response from server'));
+  });
+}
+
+const api = {
+  fetchImages,
+};
+
+export default api;
