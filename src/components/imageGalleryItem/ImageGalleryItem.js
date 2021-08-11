893576eb36b21aac3497a9515cd0e3edf6937df4
Alex34972 <alex_hifi@ukr.net>
2021-08-11 11:41:38 +0300

    Create ImageGalleryItem.js

---
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import s from './ImageGalleryItem.module.css';
+
+export default class ImageGalleryItem extends Component {
+  modalContent = id => {
+    this.props.onItemClick(id);
+  };
+  render() {
+    const { id, webformatURL, tags } = this.props;
+    return (
+      <li className={s.ImageGalleryItem}>
+        <img
+          src={webformatURL}
+          alt={tags}
+          className={s.ImageGalleryItem_image}
+          onClick={() => this.modalContent(id)}
+        />
+      </li>
+    );
+  }
+}
+ImageGalleryItem.propTypes = {
+  onItemClick: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+};
